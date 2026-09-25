343b7d5480ee504ddff94d85f1a0953ec6990552
Shrijeet Ambastha <ambasthashrijeet@gmail.com>
2026-09-25 15:48:58 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React app.</p>
+    </div>
+  );
+}
+
+export default App;
